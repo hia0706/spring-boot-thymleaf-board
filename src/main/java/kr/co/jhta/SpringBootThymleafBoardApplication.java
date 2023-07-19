4f2b8ed0cfea203ad7ca2c2d9d556366e39650e9
ljjs0121 <ljjs1215@gmail.com>
2023-07-19 10:43:30 +0900

    Initial commit

---
+++ src/main/java/kr/co/jhta/SpringBootThymleafBoardApplication.java
@@ -0,0 +1,13 @@
+package kr.co.jhta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThymleafBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootThymleafBoardApplication.class, args);
+    }
+
+}
