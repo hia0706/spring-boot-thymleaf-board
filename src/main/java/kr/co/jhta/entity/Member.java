1b7955eaf60f03e905d9a3c29f6b0f2aac43b22d
ljjs0121 <ljjs1215@gmail.com>
2023-07-19 15:00:28 +0900

    Feat: 테스트케이스 assertThat 예시

--- src/main/java/kr/co/jhta/entity/Member.java
+++ src/main/java/kr/co/jhta/entity/Member.java
@@ -1,10 +1,15 @@
 package kr.co.jhta.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.Date;
 
 @Entity
 @Table(name = "sample_board_members")
+@Getter
+@Setter
 public class Member {
 
     @Id
@@ -34,81 +39,4 @@ public class Member {
     @Temporal(TemporalType.DATE)
     private Date createdDate;
 
-    @Override
-    public String toString() {
-        return "Member{" +
-                "id='" + id + '\'' +
-                ", password='" + password + '\'' +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", tel='" + tel + '\'' +
-                ", deleted='" + deleted + '\'' +
-                ", updateDate=" + updatedDate +
-                ", createDate=" + createdDate +
-                '}';
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTel() {
-        return tel;
-    }
-
-    public void setTel(String tel) {
-        this.tel = tel;
-    }
-
-    public String getDeleted() {
-        return deleted;
-    }
-
-    public void setDeleted(String deleted) {
-        this.deleted = deleted;
-    }
-
-    public Date getUpdatedDate() {
-        return updatedDate;
-    }
-
-    public void setUpdatedDate(Date updatedDate) {
-        this.updatedDate = updatedDate;
-    }
-
-    public Date getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
 }
--- src/test/java/kr/co/jhta/SpringBootThymleafBoardApplicationTests.java
+++ src/test/java/kr/co/jhta/SpringBootThymleafBoardApplicationTests.java
@@ -2,9 +2,13 @@ package kr.co.jhta;
 
 import kr.co.jhta.entity.Member;
 import kr.co.jhta.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class SpringBootThymleafBoardApplicationTests {
@@ -12,8 +16,21 @@ class SpringBootThymleafBoardApplicationTests {
     @Autowired
     MemberRepository memberRepository;
 
+    /*
+        @BeforeEach
+            테스트에 대한 사전작업이 정의된 메소드에 지정한다.
+            이 어노테이션이 지정된 메소드에 구현된 내용은
+     */
+    @BeforeEach
+    public void setup() {
+        memberRepository.deleteAll();
+    }
+
+    @DisplayName("saveMember: 멤버추가에 성공한다.") // 메소드명 별칭
     @Test
     public void testSaveMember() {
+
+        // 테스트 준비
         Member member = new Member();
         member.setId("hong");
         member.setPassword("zxcv1234");
@@ -21,6 +38,15 @@ class SpringBootThymleafBoardApplicationTests {
         member.setEmail("hong@naver.com");
         member.setTel("010-1234-1234");
 
+        // 실행하기
         memberRepository.save(member);
+
+        // 검증하기
+        Member savedMember = memberRepository.findById("hong").orElseThrow(() -> new RuntimeException());
+
+        Assertions.assertThat(savedMember).isNotNull();
+        Assertions.assertThat(savedMember.getId()).isEqualTo("hong");
+        Assertions.assertThat(savedMember.getName()).isEqualTo("홍길동");
+
     }
 }
